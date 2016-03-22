b0aa167c0d16b5262eceed9ff2af43643a987d47
Daniel Zwelling <dzwellingmusic@gmail.com>
2016-03-21 20:04:21 -0700

    Add bridge between Entry and User(which will be created on the next commit)

--- learning_journal/models.py
+++ learning_journal/models.py
@@ -6,12 +6,14 @@ from sqlalchemy import (
     Integer,
     Unicode,
     UnicodeText,
+    ForeignKey,
     )
 from pyramid.security import Allow, Everyone
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import (
     scoped_session,
     sessionmaker,
+    relationship,
     )
 from zope.sqlalchemy import ZopeTransactionExtension
 
@@ -27,12 +29,15 @@ class Entry(Base):
     title = Column(Unicode(128), unique=True)
     text = Column(UnicodeText)
     created = Column(DateTime, default=datetime.datetime.utcnow)
-
+    author_id = Column(Integer, ForeignKey('users.id'))
+    #Ties User model to Entry model
+    author = relationship('User', back_populates='entries')
 
     @property
     def __acl__(self):
         """Add permissions for specific instance of Entry object.
-        self.author.username is the user who created this Entry instance."""
+        self.author.username is the user who created this Entry instance.
+        """
         return [
             (Allow, Everyone, 'view'),
             (Allow, self.author.username, 'edit')
