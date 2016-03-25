be4bdae0dcb1d54751d7b86fcfeb921610edac38
Daniel Zwelling <dzwellingmusic@gmail.com>
2016-03-24 18:55:02 -0700

    Fix test_model

--- learning_journal/tests/test_models.py
+++ learning_journal/tests/test_models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from learning_journal.models import Entry, DBSession
 
-def test_create_entry(dbtransaction):
+def test_create_entry(sqlengine):
     """Assert entry was entered into database."""
     new_entry = Entry(title="Entry1", text="Hey, this works. Awesome.")
     assert new_entry.id is None
